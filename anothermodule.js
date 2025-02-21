4efda204d77a3a03a8adf41ca339202d07e131e1
Samuel Lunghe <samuellunghe@gmail.com>
2025-02-21 14:29:17 +0300

    Express Project added

--- anothermodule.js
+++ anothermodule.js
@@ -42,4 +42,97 @@ let totalmem= os.totalmem();
 let freemem = os.freemem();
 // console.log(freemem);
 // console.log(totalmem);
-console.log("Hello world")
+
+
+const fs=require('fs');
+
+// fs.writeFileSync("test.txt","Hello From Node JS"); 
+// fs.appendFileSync("test.txt","\nYou know I am your friend wright?")
+// let data=fs.readFileSync("test.txt","utf-8");
+// console.log(data);
+// try {
+//     fs.unlinkSync("test.txt");
+//     console.log("Test deleted successfully");
+// } catch (error) {
+//     console.log(error);
+//     console.log("Unable to delete test.txt");
+// }
+
+//Asynchronously 
+
+// fs.writeFile("test.txt","Hello NodeJS",(error)=>{
+//     if(!error){
+//         console.log("File created successfully");
+//     }
+//     else{
+//         console.log("Error Occured. File not created successfully");
+//     }
+// });
+// console.log("Are you alright?");
+
+// fs.appendFile("test.txt","\n I am coming for you",(error)=>{
+//     if(!error) console.log("File appended successfully");
+//     else console.log("Error Occured: " + error);
+// })
+// console.log("This is Synchronous");
+
+// fs.readFile("test.txt",{encoding:"utf8"},(error,data)=>{
+//     if(error) console.log("Error: " + error);
+//     else console.log(data);
+// });
+
+
+// fs.writeFile("PCData.txt", "== PC DATA ==", (error)=>{
+//     if(error) console.log("Error Occured:Unable to write file");
+//     else console.log("PC Data Created successfully");
+// });
+
+// const osData={
+//     type: typeOfOS,
+//     architecture: architecture,
+//     user: user.username,
+  
+// }
+// console.log(osData);
+// let keys= Object.keys(osData);
+// let values= Object.values(osData);
+// for (let item=0; item<keys.length; item++){
+//     fs.appendFile("PCData.txt","\n"+String(keys[item])+":"+String(osData[keys[item]])+"\n",(error) =>{
+//         if(error) console.log(error);
+//         else console.log("Appended Successfully");
+//     });
+// }
+
+
+// ====== EVENTS MODULE ===
+const events = require("events");
+// console.log(events);
+const { EventEmitter }= events;
+const eventEmitter= new EventEmitter();
+
+eventEmitter.on("Event-1",(parm1, val, name, age)=>{
+    console.log("I am the best");
+    console.log(val);
+    console.log(name);
+    console.log(age);
+    console.log(parm1);
+});
+// eventEmitter.emit("Event-1",11,22,"gauge",56);
+// console.log("I am Synchronous");
+
+eventEmitter.on("Event-2",(obj)=>{
+    console.log(obj);
+});
+// eventEmitter.emit("Event-2",{msg:"Hello, world!", age:13, name:"Samuel Smith"});
+
+module.exports = { 'eventEmitter': eventEmitter};
+
+const readStream=fs.createReadStream("PCData.txt");
+
+readStream.on("data", function(buffer){
+    console.log(buffer);
+    console.log(buffer.toString());
+});
+
+
+
--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 const http=require('http');
+const { eventEmitter }= require("./anothermodule");
+const fs=require('fs');
+eventEmitter.emit("Event-1",11,22,"gauge",56);
+
 const server=http.createServer(function (req,res){
     if (req.url=== '/'){
         res.writeHead(200,{'Content-Type': 'text/html'});
         res.write('<html><body><h1>Hello Sam how are you doing</h1></body></html>')
-        console.log("Server responded");
+        // console.log("Server responded");
         res.end();
     }
     else if (req.url === '/samuellunghe'){
         res.writeHead(200,{'Content-Type': 'text/html'});
         res.write('<h1 style="color:#224">Your are really Samuel Lunghe The Great </h1>')
-        console.log("Server responded to /samuellunghe");
+        // console.log("Server responded to /samuellunghe");
+        res.end();
+    }
+    else if (req.url==="/mypage"){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        const data=fs.readFileSync("test.html","utf8");
+        res.write(data);
         res.end();
     }
     else{
@@ -19,7 +29,12 @@ const server=http.createServer(function (req,res){
         res.end();
     }
 })
+server.on("connection",()=>{
+    let facts=["I will become rich soon", " I will help  my friends and family", "I will make a great husband", "I will never care about money", " I am getting my master's degree soon"]
 
+    console.log("Fun Fact");
+    console.log(facts[Math.floor(Math.random()*5)])
+})
 const PORT=5000;
 server.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
